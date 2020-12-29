3c12016222ea25409aaee55255b79029f82b7cc8
Priscilla Joly <priscilla.joly@epitech.eu>
2020-12-29 14:53:36 +0100

    Ajout index : admin et pour la page d'accueil + header

--- src/main/java/com/my_restaurant/my_restaurant/controller/HomeController.java
+++ src/main/java/com/my_restaurant/my_restaurant/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.my_restaurant.my_restaurant.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
     @GetMapping("/")
-    public String hello() {
-        return String.format("HOME PAGE!");
+    public String homePage() {
+        return "index";
     }
 }
