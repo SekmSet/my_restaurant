3be7a7175a5e74e1d4e592836d3f761aec4eb726
Priscilla Joly <priscilla.joly@epitech.eu>
2020-12-31 15:09:32 +0100

    display last four avis

--- src/main/java/com/my_restaurant/my_restaurant/controller/HomeController.java
+++ src/main/java/com/my_restaurant/my_restaurant/controller/HomeController.java
@@ -1,12 +1,25 @@
 package com.my_restaurant.my_restaurant.controller;
 
+import com.my_restaurant.my_restaurant.entity.Avis;
+import com.my_restaurant.my_restaurant.service.AvisService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private AvisService avisService;
+
     @GetMapping("/")
-    public String homePage() {
+    public String homePage(Model model) {
+        List<Avis> avis = avisService.getLast(4);
+        model.addAttribute("avis", avis);
+
         return "index";
     }
 }
--- src/main/java/com/my_restaurant/my_restaurant/repository/AvisRepository.java
+++ src/main/java/com/my_restaurant/my_restaurant/repository/AvisRepository.java
@@ -1,8 +1,16 @@
 package com.my_restaurant.my_restaurant.repository;
 
 import com.my_restaurant.my_restaurant.entity.Avis;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface AvisRepository extends CrudRepository<Avis, Long> { }
+public interface AvisRepository extends CrudRepository<Avis, Long> {
+
+    @Query(value = "select * from avis a order by a.created_at desc limit :limit", nativeQuery = true)
+    public List<Avis> findOrderedBySeatNumberLimitedTo(@Param("limit") long limit);
+}
