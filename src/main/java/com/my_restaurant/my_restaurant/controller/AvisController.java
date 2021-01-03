b36ff942795ad2b38bb071d79ad97721fee6a0d0
Priscilla Joly <priscilla.joly@epitech.eu>
2021-01-03 18:27:08 +0100

    avis index + delete admin

--- src/main/java/com/my_restaurant/my_restaurant/controller/AvisController.java
+++ src/main/java/com/my_restaurant/my_restaurant/controller/AvisController.java
@@ -1,12 +1,14 @@
 package com.my_restaurant.my_restaurant.controller;
 
 import com.my_restaurant.my_restaurant.entity.Avis;
+import com.my_restaurant.my_restaurant.entity.Carte;
 import com.my_restaurant.my_restaurant.service.AvisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import javax.validation.Valid;
 
@@ -30,5 +32,20 @@ public class AvisController {
         avisService.save(avis);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/admin/avis")
+    public String indexAvis(Model model){
+        var avis = avisService.findAll();
+        model.addAttribute("avis", avis);
+        return "admin/avis/index";
+    }
+
+    @GetMapping("/admin/avis/delete/{id}")
+    public String deleteCarte(@PathVariable("id") long id, Model model) {
+        Avis avi = avisService.findById(id);
+
+        avisService.delete(avi);
+        return "redirect:/admin/avis";
     }
 }
